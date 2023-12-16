3294152914eb0dbc7108860faef4e9f2884a71d3
sakkutify <137820036+sakkutify@users.noreply.github.com>
2023-12-16 10:29:26 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
-const API_KEY = 'AIzaSyBdAAkI7sKy7Mker2N7IoKm8VwH9JTDBWk';
-const VIDEO_ID = 'XETTqjJ8mOE'; 
+const API_KEY = AIzaSyBdAAkI7sKy7Mker2N7IoKm8VwH9JTDBWk;
+const VIDEO_ID = XETTqjJ8mOE; 
 
 
 const tag = document.createElement('script');
@@ -11,25 +11,31 @@ firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 let player;
 
 
+
 function onYouTubeIframeAPIReady() {
-    player = new YT.Player('player', {
-        height: '360',
-        width: '640',
-        videoId: VIDEO_ID,
-        playerVars: {
-            'autoplay': 1,
-            'controls': 1,
-            'showinfo': 0,
-            'rel': 0,
-            'iv_load_policy': 3,
-            'modestbranding': 1
-        },
-        events: {
-            'onReady': onPlayerReady,
-            'onStateChange': onPlayerStateChange
-        }
-    });
+    try {
+        player = new YT.Player('player', {
+            height: '360',
+            width: '640',
+            videoId: VIDEO_ID,
+            playerVars: {
+                'autoplay': 1,
+                'controls': 1,
+                'showinfo': 0,
+                'rel': 0,
+                'iv_load_policy': 3,
+                'modestbranding': 1
+            },
+            events: {
+                'onReady': onPlayerReady,
+                'onStateChange': onPlayerStateChange
+            }
+        });
+    } catch (error) {
+        console.error('Error initializing YouTube Player:', error);
+    }
 }
+
 
 
 function onPlayerReady(event) {
