dda60971e6160bde6ea53eb7f29bc12b81351ebd
sakkutify <137820036+sakkutify@users.noreply.github.com>
2023-12-16 10:07:42 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,42 @@
+
+const API_KEY = 'AIzaSyBdAAkI7sKy7Mker2N7IoKm8VwH9JTDBWk';
+const VIDEO_ID = 'XETTqjJ8mOE'; 
+
+
+const tag = document.createElement('script');
+tag.src = 'https://www.youtube.com/iframe_api';
+const firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+let player;
+
+
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+        height: '360',
+        width: '640',
+        videoId: VIDEO_ID,
+        playerVars: {
+            'autoplay': 1,
+            'controls': 1,
+            'showinfo': 0,
+            'rel': 0,
+            'iv_load_policy': 3,
+            'modestbranding': 1
+        },
+        events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+        }
+    });
+}
+
+
+function onPlayerReady(event) {
+    
+}
+
+
+function onPlayerStateChange(event) {
+    
+}
